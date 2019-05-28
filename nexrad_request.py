63a15e9f34a2e27e9b36008bc6a3ab363d15d573
SurayaDev <becky@codelouisville.org>
2019-05-27 23:32:14 -0400

    Pushing script to fetch NEXRAD Level II data using nexradaws Python package

---
+++ nexrad_request.py
@@ -0,0 +1,35 @@
+import nexradaws
+import pytz
+import tempfile
+from datetime import datetime
+import pyart
+import os
+import gzip
+import matplotlib.pyplot as plt
+import pandas as pd
+import io
+import codecs
+
+# Temp location for file io
+templocation = tempfile.mkdtemp()
+
+# Establish NEXRAD Level II connection
+conn = nexradaws.NexradAwsInterface()
+
+# Preliminary params while I get familiar with the data
+eastern_timezone = pytz.timezone('US/Eastern')
+radar_id = 'KTLX'
+start = eastern_timezone.localize(datetime(2013,5,31,17,0))
+end = eastern_timezone.localize (datetime(2013,5,31,19,0))
+scans = conn.get_avail_scans_in_range(start, end, radar_id)
+
+print("There are {} scans available between {} and {}\n".format(len(scans), start, end))
+
+# Download scans based on params to temp file
+results = conn.download(scans[0:1], templocation)
+
+# Enumerate through results for each scan and evaluate reflectivity
+for i,scan in enumerate(results.iter_success(),start=1):
+    radar = pyart.io.read_nexrad_archive(scan.filepath)
+    refl_grid = radar.get_field(0, 'reflectivity')
+    # print(type(refl_grid))
